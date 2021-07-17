0a00ebf6803ab4e933871dede6dfdc1132339752
Ninad Govind Patil <ninadpatil1452@gmail.com>
2021-07-17 19:25:50 +0530

    Linked SignUp page and Login Page with each other

--- app/src/main/java/com/example/e_parking/MainActivity.kt
+++ app/src/main/java/com/example/e_parking/MainActivity.kt
@@ -1,11 +1,24 @@
 package com.example.e_parking
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
 
 class MainActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val tvRegister = findViewById<TextView>(R.id.tv_register)
+        tvRegister.setOnClickListener{
+            //Launch the registration screen when user clicks on the text
+            val intent = Intent(this,Signup_page::class.java)
+            startActivity(intent)
+        }
     }
+
 }
--- app/src/main/java/com/example/e_parking/Signup_page.kt
+++ app/src/main/java/com/example/e_parking/Signup_page.kt
@@ -1,11 +1,39 @@
 package com.example.e_parking
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class Signup_page : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        // Initialize Firebase Auth
+        auth = Firebase.auth
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_signup_page)
+
+        val tvLogin = findViewById<TextView>(R.id.tv_login)
+
+        tvLogin.setOnClickListener{
+            //Launch the Login screen when user clicks on the text
+            val intent = Intent(this,MainActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
+
+//    public override fun onStart() {
+//        super.onStart()
+//        // Check if user is signed in (non-null) and update UI accordingly.
+//        val currentUser = auth.currentUser
+//        if(currentUser != null){
+//            reload();
+//        }
+//
+//    }
