04a50324e3a83d7d0abc2b870a6c5236cc0320fe
Ninad Govind Patil <ninadpatil1452@gmail.com>
2021-07-17 14:47:22 +0530

    Sign Up page has been Added

---
+++ app/src/main/java/com/example/e_parking/Signup_page.kt
@@ -0,0 +1,11 @@
+package com.example.e_parking
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Signup_page : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_signup_page)
+    }
+}
